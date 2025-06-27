f922187966cb042bd961a911f6a59c5bd8dfdce2
Aryan kumar <aryhu@Aryans-MacBook-Air-4.local>
2025-06-28 04:48:02 +0530

    changing header name

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ const Header = () => {
         to="/products"
         className="font-serif text-3xl font-bold  relative hover:scale-105 transform transition-all duration-300 ease-in-out before:content-[''] before:absolute before:w-0 hover:before:w-[50%] before:h-[2px] before:bottom-0 before:left-[50%] before:bg-white before:origin-center before:transition-all before:duration-700 after:content-[''] after:absolute after:w-0 hover:after:w-[50%] after:h-[2px] after:bottom-0 after:right-[50%] after:bg-white after:origin-center after:transition-all after:duration-700"
       >
-        ishary
+        isharae
       </Link>
       <div></div>
       {/* Mobile Menu */}
