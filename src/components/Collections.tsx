f920a3cdf65b0f6551661b1dfcf55bb09ac59312
Aryan kumar <aryhu@Aryans-MacBook-Air-4.local>
2025-06-29 15:50:41 +0530

    soling issue in collections

--- src/components/Collections.tsx
+++ src/components/Collections.tsx
@@ -4,6 +4,8 @@ const Collections: React.FC = () => {
   const collectionItems: string[] = [
     "https://images.meesho.com/images/products/304116583/2g0gv_512.webp",
     "https://i.pinimg.com/736x/37/64/ed/3764edcd3455f35f33d53580ce597a3e.jpg",
+    "https://i.pinimg.com/736x/37/64/ed/3764edcd3455f35f33d53580ce597a3e.jpg",
+    "https://i.pinimg.com/736x/37/64/ed/3764edcd3455f35f33d53580ce597a3e.jpg",
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbntf-CnsLyS9Hwlm9KmTRE6kpB59t1eDsAg&s",
     "https://img.theloom.in/blog/wp-content/uploads/2023/09/14-03-23-918-e1695289116121.png",
     "https://img.theloom.in/blog/wp-content/uploads/2023/09/14-03-23-918-e1695289116121.png",
@@ -11,8 +13,8 @@ const Collections: React.FC = () => {
     "https://img.theloom.in/blog/wp-content/uploads/2023/09/14-03-23-918-e1695289116121.png",
   ];
 
-  const topRow = collectionItems.slice(0, 4);
-  const bottomRow = collectionItems.slice(4);
+  const topRow = collectionItems.slice(0, 6);
+  const bottomRow = collectionItems.slice(6);
 
   return (
     <div className="flex flex-col w-full bg-white py-8">
@@ -57,24 +59,32 @@ const Collections: React.FC = () => {
                 60% 85%, 70% 100%, 80% 85%, 90% 100%, 100% 85%, 100% 0%, 0% 0%
               );
             }
+
+          }
+
+          @media (max-width: 1227px) {
+            .hide-below-1228 {
+              display: none;
+            }
           }
         `}
       </style>
 
       <div className="relative overflow-hidden">
-        <div className="bg-[#CC7351] p-4 md:p-6 lg:p-8 clip-zigzag w-full h-[18rem] md:h-[26rem] lg:h-[34rem]">
+        <div className="bg-[#492822] p-4 md:p-6 lg:p-8 clip-zigzag w-full h-[18rem] md:h-[26rem] lg:h-[34rem]">
           <div className="flex flex-col gap-10 md:gap-16 lg:gap-20 items-center w-full max-w-6xl mx-auto">
             <div className="flex justify-center flex-wrap gap-4 md:gap-8 lg:gap-12">
               {topRow.map((item, index) => (
                 <div
                   key={index}
-                  className="
+                  className={`
                     bg-white rounded-full overflow-hidden
-                     w-16 h-16
+                    w-16 h-16
                     md:w-28 md:h-28
-                    lg:w-40 lg:h-40
+                    lg:w-38 lg:h-38
                     transition-all duration-300
-                  "
+                    ${index >= 4 ? "hide-below-1228" : ""}
+                  `}
                 >
                   <img
                     src={item}
@@ -88,18 +98,18 @@ const Collections: React.FC = () => {
             <div className="flex justify-center flex-wrap gap-4 md:gap-8 lg:gap-12">
               {bottomRow.map((item, index) => (
                 <div
-                  key={index + 4}
+                  key={index + 6}
                   className="
-                    bg-white rounded-full  overflow-hidden
+                    bg-white rounded-full overflow-hidden
                     w-16 h-16
                     md:w-28 md:h-28
-                    lg:w-40 lg:h-40
+                    lg:w-38 lg:h-38
                     transition-all duration-300
                   "
                 >
                   <img
                     src={item}
-                    alt={"Item " + (index + 5)}
+                    alt={"Item " + (index + 7)}
                     className="w-full h-full object-cover"
                   />
                 </div>
