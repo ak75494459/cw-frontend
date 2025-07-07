0ac9ee384157b6e96b82ceb960df92dcad7bfc32
Aryan kumar <aryhu@Aryans-MacBook-Air-4.local>
2025-07-08 02:19:38 +0530

    fixing logo again

--- src/layouts/ProductsLayout.tsx
+++ src/layouts/ProductsLayout.tsx
@@ -12,7 +12,7 @@ const Layout = ({ children }: Props) => {
   return (
     <div className="flex flex-col min-h-screen">
       <header
-        className="w-full py-4 text-[white] top-0 left-0 z-50  transition-all bg-[#492822] shadow-md backdrop-blur-md 
+        className="w-full py-3 text-[white] top-0 left-0 z-50  transition-all bg-[#492822] shadow-md backdrop-blur-md 
           "
       >
         <ProductsHeader />
