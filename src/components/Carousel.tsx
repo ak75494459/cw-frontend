654a441e1b1933e2269731be44846a8d951ab7ad
Aryan kumar <aryhu@Aryans-MacBook-Air-4.local>
2025-07-19 05:54:06 +0530

    making responsive

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -14,7 +14,7 @@ const products = [
   {
     id: 1,
     image:
-      "https://res.cloudinary.com/dmv8kh0yx/image/upload/v1752501648/ChatGPT_Image_Jul_14_2025_at_07_30_30_PM_nlem2s.png",
+      "https://res.cloudinary.com/dmv8kh0yx/image/upload/v1752882239/Raksha_3_swuual.png",
     hoverImage:
       "https://res.cloudinary.com/dmv8kh0yx/image/upload/v1752266845/stcfzxbuvuioajgc5zin.jpg",
   },
@@ -56,7 +56,7 @@ export function MyCarousel() {
       <div className="text-center mt-10 mb-4">
         <h1 className="text-4xl font-bold text-[#333] tracking-wide relative inline-block">
           New Collections
-          <span className="block h-[3px] bg-[#99775C] mt-2 w-1/2 mx-auto rounded-full"></span>
+          <span className="block h-[3px] bg-[#99775C] mt-2 w-1/2  rounded-full"></span>
         </h1>
         <p className="text-gray-500 mt-2 text-sm">
           Discover our latest arrivals curated with style
@@ -79,9 +79,9 @@ export function MyCarousel() {
             {products.map((product) => (
               <CarouselItem
                 key={product.id}
-                className="carousel-item basis-1/4 h-full"
+                className="carousel-item basis-1/4 h-[100]"
               >
-                <div className="p-4 w-full h-[500px] sm:h-[400px] md:h-[400px] overflow-hidden relative">
+                <div className="p-4 w-full h-[400px]  overflow-hidden relative">
                   <div className="w-full h-full relative overflow-hidden">
                     <img
                       src={product.image}
@@ -112,18 +112,116 @@ export function MyCarousel() {
 
       <style>{`
         .carousel-item {
-          flex: 0 0 25%;
+          flex: 0 0 30%;
         }
 
-        @media (max-width: 1000px) {
+        @media (max-width: 1300px) {
           .carousel-item {
-            flex: 0 0 50%;
+            flex: 0 0 33%;
+          }
+        }
+           @media (max-width: 1226px) {
+          .carousel-item {
+            flex: 0 0 38%;
           }
         }
 
+         @media (max-width: 1058px) {
+          .carousel-item {
+            flex: 0 0 42%;
+          }
+        }
+           @media (max-width: 974px) {
+          .carousel-item {
+            flex: 0 0 45%;
+          }
+        }
+        
+          @media (max-width: 882px) {
+          .carousel-item {
+            flex: 0 0 48%;
+          }
+        }
+
+         @media (max-width: 845px) {
+          .carousel-item {
+            flex: 0 0 52%;
+          }
+        }
+
+         @media (max-width: 778px) {
+          .carousel-item {
+            flex: 0 0 55%;
+          }
+        }
+
+         @media (max-width: 725px) {
+          .carousel-item {
+            flex: 0 0 57%;
+          }
+        }
+
+        @media (max-width: 699px) {
+          .carousel-item {
+            flex: 0 0 60%;
+          }
+        }
+
+         @media (max-width: 665px) {
+          .carousel-item {
+            flex: 0 0 63%;
+          }
+        }
+
+         @media (max-width: 622px) {
+          .carousel-item {
+            flex: 0 0 65%;
+          }
+        }
+
+
+
         @media (max-width: 600px) {
           .carousel-item {
+            flex: 0 0 70%;
+          }
+        }
+        
+         @media (max-width: 578px) {
+          .carousel-item {
+            flex: 0 0 72%;
+          }
+        }
+           @media (max-width: 544px) {
+          .carousel-item {
+            flex: 0 0 80%;
+          }
+        }
+            @media (max-width: 486px) {
+          .carousel-item {
+            flex: 0 0 83%;
+          }
+        }
+           @media (max-width: 476px) {
+          .carousel-item {
             flex: 0 0 100%;
+          }
+        }
+
+         @media (max-width: 443px) {
+          .carousel-item {
+            flex: 0 0 100%;
+          }
+        }
+           @media (max-width: 354px) {
+          .carousel-item {
+            flex: 0 0 110%;
+            marginRight: 0;
+          }
+        }
+           @media (max-width: 360px) {
+          .carousel-item {
+            flex: 0 0 120%;
           }
         }
 
